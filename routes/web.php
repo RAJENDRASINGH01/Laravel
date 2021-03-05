206151e31f2b4f503e407fc8e68affa738bab7f8
RAJENDRASINGH01 <singh0rajendra0890@gmail.com>
2021-03-05 08:35:11 +0545

    third commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/ktm', function () {
     return view('welcome');
 });
+// Route::get('/hello', function () {
+//     return view('firstpage');
+// });
+
+Route::get('/world', function () {
+    return view('secondpage');
+});
+
+Route::get('/hello','App\Http\Controllers\myController@index');
